41ef040b61d74c35b54e054a1f574f474a6c9a5e
RishiS08 <Singhal.rishi07@gmail.com>
2026-05-11 14:03:44 +0530

    Add files via upload

---
+++ Q22.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+class Pattern {
+public:
+    void hollowDiamond(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) cout << " ";
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                if (j == 1 || j == 2 * i - 1) cout << "*";
+                else cout << " ";
+            }
+            cout << endl;
+        }
+
+        for (int i = n - 1; i >= 1; i--) {
+            for (int j = 1; j <= n - i; j++) cout << " ";
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                if (j == 1 || j == 2 * i - 1) cout << "*";
+                else cout << " ";
+            }
+            cout << endl;
+        }
+    }
+};
+
+int main(){
+    Pattern obj;
+    obj.hollowDiamond(5);
+
+    return 0;
+}
+
